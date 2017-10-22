4b534bfb777555ab63db8099de0c7ed669f1f0a5
tkttkt <tkt39@hotmail.com>
2017-10-23 03:58:00 +0900

    ifoo

--- app.js
+++ app.js
@@ -104,6 +104,6 @@ app.use('/articles', articles);
 app.use('/users', users);
 
 // Start Server
-app.listen(3110, function(){
-  console.log('Server started on port 3110...');
+app.listen(80, function(){
+  console.log('Server started on port 80...');
 });
